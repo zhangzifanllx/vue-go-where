6004134eb3d6e71bfba83d7b7656e8ab3ef304e8
chenlie <1183738806@qq.com>
2021-09-19 15:52:29 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import './assets/styles/reset.css' // 统一不同手机初始化样式
+import './assets/styles/border.css' // 一像素解决方案
+import fastclick from 'fastclick'
+fastclick.attach(document.body) // 解决移动端点击300ms延迟问题
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -13,3 +16,4 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+// 路由就是根据网址的不同，返回不同的内容给用户
---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'Home'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
