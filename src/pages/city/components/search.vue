20a7e4b65cda22097db316dfe2bfece3552b0882
chenlie <1183738806@qq.com>
2021-09-23 08:47:36 +0800

    city-search

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="请输入城市或者拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/varible.styl'
+  .search
+    background-color: $bgColor
+    padding: .1rem
+    input
+        width: 100%
+        text-align: center
+        box-sizing: border-box
+        color: #666
+        padding: 0 .2rem
+</style>
